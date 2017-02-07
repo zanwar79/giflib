0dd20bdd3ea75d1f121ab1aa50d3f8be885a09c1
SWEngatFontbonne <u970133@yahoo.com>
2017-02-07 14:17:19 -0600

    Favourites Functionality Added

--- src/main/java/com/sweng/giflib/controller/GifController.java
+++ src/main/java/com/sweng/giflib/controller/GifController.java
@@ -9,6 +9,7 @@ import com.sweng.giflib.model.Gif;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -26,9 +27,9 @@ public class GifController {
         return "home";
     }
 
-    @RequestMapping("/gif")
-    public String gifdetails(ModelMap modelMap) {
-        Gif gif = gifRepository.findByName("android-explosion");
+    @RequestMapping("/gif/{name}")
+    public String gifdetails(@PathVariable String name, ModelMap modelMap) {
+        Gif gif = gifRepository.findByName(name);
         modelMap.put("gif", gif);
         return "gif-details";
     }
