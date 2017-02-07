6e03b4561bbd0ec326be1f70503e1fcc7db3511b
SWEngatFontbonne <u970133@yahoo.com>
2017-02-06 20:26:10 -0600

    Feeding Data to Our Thymeleaf Templates

--- src/main/java/com/sweng/giflib/controller/GifController.java
+++ src/main/java/com/sweng/giflib/controller/GifController.java
@@ -4,9 +4,13 @@ package com.sweng.giflib.controller;
  * Created by ZAnwar on 1/29/2017.
  */
 
+import com.sweng.giflib.model.Gif;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.time.LocalDate;
 
 @Controller
 public class GifController {
@@ -16,10 +20,13 @@ public class GifController {
     }
 
     @RequestMapping("/gif")
-    @ResponseBody
-    public String listSpecificGif() {
-        return "This is my GIF!";
+    //@ResponseBody
+    public String gifdetails(ModelMap modelMap) {
+        Gif gif = new Gif("compiler-bot", LocalDate.of(2015, 2, 13), "Zahid Anwar", true);
+        modelMap.put("gif", gif);
+        return "gif-details";
     }
+
 }
 
 
