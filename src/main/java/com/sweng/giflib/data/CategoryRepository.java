759a69f542fd3159dd4c73dfdc2748e4ac2f51d5
SWEngatFontbonne <u970133@yahoo.com>
2017-02-15 22:58:16 -0600

    Added Category Model and CategoryRepository

---
+++ src/main/java/com/sweng/giflib/data/CategoryRepository.java
@@ -0,0 +1,33 @@
+package com.sweng.giflib.data;
+
+import com.sweng.giflib.model.Category;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by ZAnwar on 2/14/2017.
+ */
+public class CategoryRepository {
+    private static final List<Category> ALL_CATEGORIES = Arrays.asList(
+            new Category(1, "Technology"),
+            new Category(2,"People"),
+            new Category(3,"Destruction")
+
+    );
+
+    public List<Category> getAllCategories(){
+        return ALL_CATEGORIES;
+    }
+
+    public Category findById(int id) {
+        for(Category category : ALL_CATEGORIES) {
+            if(category.getId() == id){
+                return category;
+            }
+        }
+        return null;
+    }
+
+}
+
