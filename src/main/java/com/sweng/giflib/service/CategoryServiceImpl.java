3a033815b1c2d24e978fc7ec0c001533972296c8
SWEngatFontbonne <u970133@yahoo.com>
2017-03-05 23:34:27 -0600

    implemented a post-redirect-get pattern in Spring Created forms in Thymeleaf for adding new category.

--- src/main/java/com/sweng/giflib/service/CategoryServiceImpl.java
+++ src/main/java/com/sweng/giflib/service/CategoryServiceImpl.java
@@ -28,7 +28,7 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public void save(Category category) {
-
+        categoryDao.save(category);
     }
 
     @Override
