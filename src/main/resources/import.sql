232571bc4eff6a7bdde9a1f06042817ba5df14e3
SWEngatFontbonne <u970133@yahoo.com>
2017-04-11 14:04:52 -0500

    Removed Tasks and cleaner code,

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -5,8 +5,3 @@ insert into role (name) values ('ROLE_USER');
 insert into user (username,enabled,password,role_id) values ('user',true,'password',1);
 insert into user (username,enabled,password,role_id) values ('user2',true,'password',1);
 
--- Insert tasks
-insert into task (complete,description, user_id) values (true,'Code Task entity',1);
-insert into task (complete,description, user_id) values (false,'Discuss users and roles',1);
-insert into task (complete,description, user_id) values (false,'Enable Spring Security',2);
-insert into task (complete,description, user_id) values (false,'Test application',2);
